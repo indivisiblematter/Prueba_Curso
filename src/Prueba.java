45806a37461824f6e57e3c8b85481b595bb0b7b4
indivisiblematter <115508922+indivisiblematter@users.noreply.github.com>
2023-03-23 20:57:47 +0100

    Segundo commit develop. emma

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,6 +3,8 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola tú");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola tú");
 	}
 	
 }
